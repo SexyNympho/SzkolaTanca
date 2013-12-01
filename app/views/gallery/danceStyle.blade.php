56abf49957fc98fa96393b044baa132d4a53edb8
Tarnas <tarnas14@gmail.com>
2013-12-01 14:09:29 +0100

    something in dance styles

--- app/views/gallery/danceStyle.blade.php
+++ app/views/gallery/danceStyle.blade.php
@@ -2,7 +2,9 @@
 
 @section('galleryMenuLinks')
     @foreach($styles as $style)
-    <li @if($style->id == $selectedStyle) class="selected" @endif >{{ link_to_route('danceStyleGallery', $style->name, array('danceStyle' => $style->id)) }}</li>
+        <li @if($style->id == $selectedStyle) class="selected" @endif >
+             {{ link_to_route('danceStyleGallery', $style->name, array('danceStyle' => $style->id)) }}
+        </li>
     @endforeach
 @stop
 
@@ -10,6 +12,7 @@
     @if (count($galleryData) == 0)
     <p>no images to show</p>
     @endif
+    
     @foreach($galleryData as $eventGallery)
     <a href="{{ route('eventGallery', array('danceStyle' => $selectedStyle, 'danceEvent' => $eventGallery['eventId'])) }}">
         @include('images/image', array('photo' => $eventGallery['photo'], 'caption' => $eventGallery['caption']))
